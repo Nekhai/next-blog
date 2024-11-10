4f769d0390b5120eddda2e442887e6c5fed8ead4
Yurii Nekhai <yurii.nekhai@plus4u.net>
2024-11-10 22:27:48 +0200

    add code for test deploy

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,7 +1,33 @@
-export default function ShopPage() {
+// export default function ShopPage() {
+//   return (
+//     <div className="page-height pt-20 text-xl text-center">
+//       Under construction
+//     </div>
+//   );
+// }
+
+import { list } from '@vercel/blob';
+import Image from 'next/image';
+
+export default async function ShopPage() {
+  async function allImages() {
+    const blobs = await list();
+    return blobs;
+  }
+  const images = await allImages();
+
   return (
-    <div className="page-height pt-20 text-xl text-center">
-      Under construction
-    </div>
+    <section>
+      {images.blobs.map((image) => (
+        <Image
+          priority
+          key={image.pathname}
+          src={image.url}
+          alt="Image"
+          width={200}
+          height={200}
+        />
+      ))}
+    </section>
   );
 }
--- src/components/CreateForm/CreateForm.tsx
+++ src/components/CreateForm/CreateForm.tsx
@@ -4,13 +4,15 @@ import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 
+import { FileUploader } from '@/components/CreateForm/FileUploader';
+
 export const CreateForm = () => {
   const { data: session } = useSession();
   const router = useRouter();
 
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
-  const [imageUrl, setImageUrl] = useState('');
+  const [files, setFiles] = useState([]);
 
   const handleSubmit = async (e: React.SyntheticEvent) => {
     e.preventDefault();
@@ -29,45 +31,48 @@ export const CreateForm = () => {
   };
 
   return (
-    <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
-      <input
-        className="p-2 rounded text-primary-dark"
-        onChange={(e) => setTitle(e.target.value)}
-        placeholder="Title"
-        type="text"
-        value={title}
-      />
-      <textarea
-        className="p-2 rounded text-primary-dark"
-        cols={50}
-        onChange={(e) => setContent(e.target.value)}
-        placeholder="Content"
-        rows={16}
-        value={content}
-      />
-      <input
-        className="p-2 rounded text-primary-dark"
-        onChange={(e) => setImageUrl(e.target.value)}
-        placeholder="Url to the image"
-        type="url"
-        value={imageUrl}
-      />
-      <div className="flex justify-end gap-5">
-        <button
-          className="p-2 hover:text-active"
-          onClick={() => router.push('/')}
-          type="button"
-        >
-          Cancel
-        </button>
-        <button
-          className="p-2 active:hover:text-active disabled:text-gray-400"
-          disabled={!content || !title}
-          type="submit"
-        >
-          Create
-        </button>
-      </div>
-    </form>
+    <>
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+        <input
+          className="p-2 rounded text-primary-dark"
+          onChange={(e) => setTitle(e.target.value)}
+          placeholder="Title"
+          type="text"
+          value={title}
+        />
+        <textarea
+          className="p-2 rounded text-primary-dark"
+          cols={50}
+          onChange={(e) => setContent(e.target.value)}
+          placeholder="Content"
+          rows={16}
+          value={content}
+        />
+        {/* <input */}
+        {/*  className="p-2 rounded text-primary-dark" */}
+        {/*  onChange={(e) => setImageUrl(e.target.value)} */}
+        {/*  placeholder="Url to the image" */}
+        {/*  type="url" */}
+        {/*  value={imageUrl} */}
+        {/* /> */}
+        <div className="flex justify-end gap-5">
+          <button
+            className="p-2 hover:text-active"
+            onClick={() => router.push('/')}
+            type="button"
+          >
+            Cancel
+          </button>
+          <button
+            className="p-2 active:hover:text-active disabled:text-gray-400"
+            disabled={!content || !title}
+            type="submit"
+          >
+            Create
+          </button>
+        </div>
+      </form>
+      <FileUploader files={files} setFiles={setFiles} />
+    </>
   );
 };
