6151c8a8dc281c8721356f308e69134f353870ef
Yurii Nekhai <yurii.nekhai@plus4u.net>
2024-11-10 22:26:28 +0200

    add blob storage

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['images.unsplash.com']
+        // domains: ['images.unsplash.com']
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'https://cqukvue0lnq2gbcx.public.blob.vercel-storage.com',
+                port: '',
+            },
+        ],
     }
 };
 
